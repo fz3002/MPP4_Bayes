05dd39f4d4242ef68c7667c4613dd49daf1e9603
fz3002 <franc3002@gmail.com>
2024-04-21 16:44:01 +0200

    Clean up

--- main.py
+++ main.py
@@ -3,77 +3,95 @@
 import csv
 
 class Bayes:
-    """_summary_
-    """
-    def __init__(self,train_set):
-        self.train_set = train_set
-        self.list_of_attributes = self.__get_attributes()
+    """Class containing Bayes learning algorithm"""
+    def __init__(self,training_set):
+        self.train_set = training_set
+        self.list_of_attributes_values = self.__get_attributes_values()
         self.decision_attribute_count = self.__count_decision_attributes()
         self.dic_for_decision_attributes = self.__train()
-        
-        
-    def __get_attributes(self):
-        """_summary_
+
+
+    def __get_attributes_values(self):
+        """Finds possible values for each attribute
 
         Returns:
-            _description_
+            List of possible attribute values
         """
         list_result = []
-        for i in range(len(self.train_set[0])-1):
+        for k in range(len(self.train_set[0])-1):
             attributes = []
             for line in self.train_set:
-                if line[i] not in attributes:
-                    attributes.append(line[i])
+                if line[k] not in attributes:
+                    attributes.append(line[k])
             list_result.append(attributes)
         return list_result
-    
+
     def __count_decision_attributes(self):
         result = {}
         for line in self.train_set:
             result[line[-1]] = result.get(line[-1],0) + 1
         return result
-    
+
     def __train(self):
-        """_summary_
+        """Finds count of each attribute value for each decision attributes
+
+        Returns:
+            Dictionary of with keys being each decision_attribute and 
+            value being another dictionary with each column index as key and 
+            another value being another dictionary with count for each value of attribute
+            like:
+            {'tak':
+                    0:
+                        {
+                         "value": 1,
+                         "value": 2
+                        }
+            }
         """
         dic_result = {}
         for decision_attribute in list(self.decision_attribute_count.keys()):
             dic_result[decision_attribute] = {}
-            for i, attribute_list in enumerate(self.list_of_attributes):
-                dic_result[decision_attribute][i] = {}
+            for k, attribute_list in enumerate(self.list_of_attributes_values):
+                dic_result[decision_attribute][k] = {}
                 for attr in attribute_list:
-                    dic_result[decision_attribute][i][attr] = 0
+                    dic_result[decision_attribute][k][attr] = 0
 
-        
+
         for decision_attribute, freq_attr_dict in dic_result.items():
             for column_index, freq_dict in freq_attr_dict.items():
                 for line in self.train_set:
                     if line[-1] == decision_attribute:
                         freq_dict[line[column_index]] += 1
-                
+
         return dic_result
-    
-    def compute(self, vector):
-        """_summary_
+
+    def compute(self, test_vector):
+        """Computes decision attribute for test vector using naive bayes method
 
         Arguments:
-            vector -- _description_
+            test_vector -- vector with values for which decision is being made
+
+        Returns:
+            Value of decision attribute with biggest probability
         """
         decision_attributes = list(self.decision_attribute_count.keys())
-        results = [self.decision_attribute_count[e]/(len(self.train_set)) for e in decision_attributes]
-        for i, result in enumerate(results):
-            for j, attribute in enumerate(self.list_of_attributes):
-                freq = self.dic_for_decision_attributes[decision_attributes[i]][j].get(vector[j], 0)
+        results = [self.decision_attribute_count[e]/(len(self.train_set))
+                   for e in decision_attributes]
+        for k, result in enumerate(results):
+            for j, attribute in enumerate(self.list_of_attributes_values):
+                freq_dict = self.dic_for_decision_attributes[decision_attributes[k]][j]
+                decision_attribute_count = self.decision_attribute_count[decision_attributes[k]]
+                freq = freq_dict.get(test_vector[j], 0)
                 if freq == 0:
-                    probability = (freq + 1) / (self.decision_attribute_count[decision_attributes[i]] + len(attribute))
+                    probability = (freq + 1) / ( decision_attribute_count + len(attribute))
                 else:
-                    probability = freq / self.decision_attribute_count[decision_attributes[i]]
+                    probability = freq / self.decision_attribute_count[decision_attributes[k]]
 
-                results[i] *= probability
+                results[k] *= probability
 
         return decision_attributes[results.index(max(results))]
-    
-            
+
+
 def read_file(file_name):
     """This is a function that reads CSV file and returns list of lines.
 
@@ -103,12 +121,10 @@ bayes = Bayes(train_set)
 
 for vector in test_set:
     print(vector, " - decision: ", bayes.compute(vector))
-    
+
 while True:
     vector_to_test = []
-    for i in range(len(bayes.list_of_attributes)):
+    for i in range(len(bayes.list_of_attributes_values)):
         vector_to_test.append(input("Enter attribute: "))
     print(bayes.compute(vector_to_test))
-    
-    
         
